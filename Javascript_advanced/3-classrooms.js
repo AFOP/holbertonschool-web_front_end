471d00563bd7ead6153a21a8c594dea7f2367b88
AFOP <felipe@ASUSAFOP.localdomain>
2023-05-26 21:55:06 -0500

    [ADD] task 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,20 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+      return function() {
+        return seat;
+      };
+    }
+  
+    var students = [];
+  
+    for (var i = 0; i < numbersOfStudents; i++) {
+        var seatNumber = i + 1;
+        students.push(studentSeat(seatNumber));
+    }
+  
+    return students;
+}
+
+var classRoom = createClassRoom(10);
+  
+console.log(classRoom[3]());
