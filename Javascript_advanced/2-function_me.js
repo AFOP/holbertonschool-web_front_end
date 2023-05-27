8b44c730e6a4f7103d289ed248f6feb225b5db1a
AFOP <felipe@ASUSAFOP.localdomain>
2023-05-26 21:04:12 -0500

    [ADD] task 2

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -3,3 +3,7 @@ function welcomeMessage(fullName) {
     alert("Welcome " + fullName);
   };
 }
+
+var guillaume = welcomeMessage("Guillaume");
+var alex = welcomeMessage("Alex");
+var fred = welcomeMessage("Fred");
