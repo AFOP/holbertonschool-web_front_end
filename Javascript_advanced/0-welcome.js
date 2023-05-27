5a6738474b6e4ca1b559f56e88a3d8020f725659
AFOP <felipe@ASUSAFOP.localdomain>
2023-05-26 20:46:52 -0500

    [ADD] task 0

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,13 @@
+var fullName="fullName is not defined";
+
+function welcome(firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+    
+    function displayFullName() {
+       alert("Welcome " + fullName + "!");
+    }
+  
+    displayFullName();
+}
+
+welcome('Holberton', 'School');
